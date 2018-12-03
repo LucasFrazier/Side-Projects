2db312352a45a73a59bd09ebcae975e8ae88c8c0
Lucas Frazier <lucasfrazier1@gmail.com>
2018-12-03 16:07:43 -0500

    Working on Jeopardy

---
+++ Jeopardy/Jeopardy/Jeopardy/Scripts/custom/categories.js
@@ -0,0 +1,63 @@
+﻿$(document).ready(function () {
+
+        $.ajax({
+            url: "http://jservice.io/api/categories",
+            type: "GET",
+            dataType: "json",
+            data: {
+                count: "20"
+            }
+        }).done(function (data) {
+            console.log(data);
+            $("#CategorySelect1").empty();
+            $("#CategorySelect2").empty();
+            $("#CategorySelect3").empty();
+            for (let x = 0; x < 20; x++) {
+                let category1 = $("<option>").text(data[x].title).attr("data", data[x].id);
+                let category2 = $("<option>").text(data[x].title).attr("data", data[x].id);
+                let category3 = $("<option>").text(data[x].title).attr("data", data[x].id);
+                $("#CategorySelect1").append(category1);
+                $("#CategorySelect2").append(category2);
+                $("#CategorySelect3").append(category3);
+            }
+
+            
+        }).fail(function (xhr, status, error) {
+            console.log(error);
+        });
+
+    $("#submitCategories").on("click", function (event) {
+
+        let userInput = $("#submitCategories").data();
+
+        $.ajax({
+            url: "https://dog.ceo/api/breeds/image/random/" + userInput,
+            type: "GET",
+            dataType: "json"
+        }).done(function (data) {
+            console.log(data);
+            $("#apiMagic").empty();
+            for (let x = 0; x < userInput; x++) {
+                let flipCard = $("<div>").addClass("flip-card");
+                let flipCardInner = $("<div>").addClass("flip-card-inner");
+                let flipCardFront = $("<div>").addClass("flip-card-front");
+                let flipCardBack = $("<div>").addClass("flip-card-back");
+                let albumImage = $("<img>").attr("src", data.message[x]).attr("style", "width:300px;height:auto;");
+
+                flipCardFront.append(albumImage);
+                flipCardInner.append(flipCardFront);
+
+                flipCardBack.append("<h1>").text("Album Name");
+                flipCardBack.append("<p>").text("Year");
+                flipCardBack.append("<p>").text("Record Label");
+                flipCardInner.append(flipCardBack);
+
+                flipCard.append(flipCardInner);
+                $("#apiMagic").append(flipCard);
+            }
+        }).fail(function (xhr, status, error) {
+            console.log(error);
+        });
+    });
+    
+});
--- iTunesAPIGame/index.js
+++ iTunesAPIGame/index.js
@@ -1,6 +1,40 @@
 $(document).ready(function () {  
 
-$("#btnSearch").on("click", function (event) {
+    $("#btnSearch").on("click", function (event) {
+
+        var userInput = $("#userInput").val();
+        
+        $.ajax({
+            url: "https://dog.ceo/api/breeds/image/random/" + userInput,
+            type: "GET",
+            dataType: "json"
+        }).done(function (data) {
+            console.log(data);
+            $("#apiMagic").empty();
+            for(let x = 0; x < userInput; x++){
+                let flipCard = $("<div>").addClass("flip-card");
+                let flipCardInner = $("<div>").addClass("flip-card-inner");
+                let flipCardFront = $("<div>").addClass("flip-card-front");
+                let flipCardBack = $("<div>").addClass("flip-card-back");
+                let albumImage = $("<img>").attr("src", data.message[x]).attr("style", "width:300px;height:auto;");
+                
+                flipCardFront.append(albumImage);
+                flipCardInner.append(flipCardFront);
+    
+                flipCardBack.append("<h1>").text("Album Name");
+                flipCardBack.append("<p>").text("Year");
+                flipCardBack.append("<p>").text("Record Label");
+                flipCardInner.append(flipCardBack);
+    
+                flipCard.append(flipCardInner);
+                $("#apiMagic").append(flipCard);
+            }
+        }).fail(function (xhr, status, error) {
+            console.log(error);
+        });
+    });
+    
+/* $("#btnSearch").on("click", function (event) {
 
     var artist = $("#artist").val();
     
@@ -12,14 +46,33 @@ $("#btnSearch").on("click", function (event) {
             term: artist,
             country: 'US',
             entity: 'album'
-
         }
     }).done(function (data) {
         console.log(data);
-                    
+        $("#apiMagic").empty();
+        for(let x = 0; x < data.results.length; x++){
+            let flipCard = $("<div>").addClass("flip-card");
+            let flipCardInner = $("<div>").addClass("flip-card-inner");
+            let flipCardFront = $("<div>").addClass("flip-card-front");
+            let flipCardBack = $("<div>").addClass("flip-card-back");
+            let albumImage = $("<img>").attr("src", data.results[x].artworkUrl100).attr("style", "width:300px;height:300px;");
+            
+            flipCardFront.append(albumImage);
+            flipCardInner.append(flipCardFront);
+
+            flipCardBack.append("<h1>").text("Album Name");
+            flipCardBack.append("<p>").text("Year");
+            flipCardBack.append("<p>").text("Record Label");
+            flipCardInner.append(flipCardBack);
+
+            flipCard.append(flipCardInner);
+            $("#apiMagic").append(flipCard);
+        }
     }).fail(function (xhr, status, error) {
         console.log(error);
     });
-});
+}); */
+
+
 
 });
